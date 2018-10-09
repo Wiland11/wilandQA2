d8aa47f90e2765d50470e6a67973c4210fefb0cc
Wiland <jurijs.uljanovs@gmail.com>
2018-10-09 21:43:54 +0300

    initial commit

---
+++ src/test/java/JavaBasics.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JavaBasics {
+    public Integer sumToCheck = 100;
+
+    @Test
+    public void firstTest() {
+        Integer a = 85;
+        Integer b = 15;
+        Integer sumToCheck = 100;
+
+        Integer c = 45;
+        Integer d = 55;
+
+        sumAndCheck(a, b);
+        sumAndCheck(c, d);
+    }
+
+    private void sumAndCheck(Integer a, Integer b) {
+        Integer sum = a + b;
+        Assertions.assertEquals(sumToCheck, sum, "Numbers not equal");
+    }
+}
