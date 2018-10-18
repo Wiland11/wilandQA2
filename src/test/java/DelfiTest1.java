472702b348badc54fae8f354e95ce964a3121cef
Wiland <jurijs.uljanovs@gmail.com>
2018-10-18 21:45:43 +0300

    Commit 7

--- src/test/java/DelfiTest1.java
+++ src/test/java/DelfiTest1.java
@@ -5,14 +5,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class DelfiTest1 {
     private final By WEB_ARTICLE_LIST = By.xpath(".//a[@class='top2012-title']");
     private final By MOB_ARTICLE_LIST = By.xpath(".//a[@class='md-scrollpos']");
 
     @Test
-    public void delfiFiveTitleTest () {
+    public void delfiFiveTitleTest() {
 
         System.setProperty("webdriver.chrome.driver", "c:/chromedriver.exe");
         WebDriver browser = new ChromeDriver();
@@ -20,46 +23,53 @@ public class DelfiTest1 {
         browser.get("http://rus.delfi.lv/");
 
         List<WebElement> webArticleList = browser.findElements(WEB_ARTICLE_LIST);
+        List<String> webArticleText = new ArrayList<String>();
+
+        webArticleText.add(webArticleList.get(0).getText());
+        webArticleText.add(webArticleList.get(1).getText());
+        webArticleText.add(webArticleList.get(2).getText());
+        webArticleText.add(webArticleList.get(3).getText());
+        webArticleText.add(webArticleList.get(4).getText());
+
+        System.out.println(webArticleText.get(2));
 
 /*        for (WebElement element: webArticleList) {
             System.out.println(element.getText());
         }*/
 
-        WebElement webFirstArticle = webArticleList.get(0);
-        WebElement webSecondArticle = webArticleList.get(1);
-        WebElement webThirdArticle = webArticleList.get(2);
-        WebElement webFourthArticle = webArticleList.get(3);
-        WebElement webFifthArticle = webArticleList.get(4);
+/*        String webFirstArticle = webArticleList.get(0).getText();
+        String webSecondArticle = webArticleList.get(1).getText();
+        String webThirdArticle = webArticleList.get(2).getText();
+        String webFourthArticle = webArticleList.get(3).getText();
+        String webFifthArticle = webArticleList.get(4).getText();*/
 
-        String webFirstArticleText = webFirstArticle.getText();
-        String webSecondArticleText = webSecondArticle.getText();
-        String webThirdArticleText = webThirdArticle.getText();
-        String webFourthArticleText = webFourthArticle.getText();
-        String webFifthArticleText = webFifthArticle.getText();
-
-//        System.out.println(webFirstArticle);
+//        System.out.println(webArticleListText);
 
         browser.get("http://m.rus.delfi.lv/");
 
         List<WebElement> mobArticleList = browser.findElements(MOB_ARTICLE_LIST);
+        List<String> mobArticleText = new ArrayList<String>();
 
-        WebElement mobFirstArticle = mobArticleList.get(0);
-        WebElement mobSecondArticle = mobArticleList.get(1);
-        WebElement mobThirdArticle = mobArticleList.get(2);
-        WebElement mobFourthArticle = mobArticleList.get(3);
-        WebElement mobFifthArticle = mobArticleList.get(4);
+        mobArticleText.add(mobArticleList.get(0).getText());
+        mobArticleText.add(mobArticleList.get(1).getText());
+        mobArticleText.add(mobArticleList.get(2).getText());
+        mobArticleText.add(mobArticleList.get(3).getText());
+        mobArticleText.add(mobArticleList.get(4).getText());
 
-        String mobFirstArticleText = mobFirstArticle.getText();
-        String mobSecondArticleText = mobSecondArticle.getText();
-        String mobThirdArticleText = mobThirdArticle.getText();
-        String mobFourthArticleText = mobFourthArticle.getText();
-        String mobFifthArticleText = mobFifthArticle.getText();
+        System.out.println(mobArticleText.get(2));
 
-        Assertions.assertEquals(webFirstArticleText, mobFirstArticleText, "first article not same");
-        Assertions.assertEquals(webSecondArticleText, mobSecondArticleText, "second article not same");
-        Assertions.assertEquals(webThirdArticleText, mobThirdArticleText, "third article not same");
-        Assertions.assertEquals(webFourthArticleText, mobFourthArticleText, "fourth article not same");
-        Assertions.assertEquals(webFifthArticleText, mobFifthArticleText, "fifth article not same");
+
+/*        String mobFirstArticle = mobArticleList.get(0).getText();
+        String mobSecondArticle = mobArticleList.get(1).getText();
+        String mobThirdArticle = mobArticleList.get(2).getText();
+        String mobFourthArticle = mobArticleList.get(3).getText();
+        String mobFifthArticle = mobArticleList.get(4).getText();
+
+        Assertions.assertEquals(webFirstArticle, mobFirstArticle, "first article not same");
+        Assertions.assertEquals(webSecondArticle, mobSecondArticle, "second article not same");
+        Assertions.assertEquals(webThirdArticle, mobThirdArticle, "third article not same");
+        Assertions.assertEquals(webFourthArticle, mobFourthArticle, "fourth article not same");
+        Assertions.assertEquals(webFifthArticle, mobFifthArticle, "fifth article not same");*/
 
         browser.close();
     }
