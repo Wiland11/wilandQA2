f38f30464e98cf528e196c71e5f6ecb0b8eca985
Wiland <jurijs.uljanovs@gmail.com>
2018-10-23 19:41:37 +0300

    initial commit 9

--- src/test/java/DelfiTest1.java
+++ src/test/java/DelfiTest1.java
@@ -9,33 +9,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DelfiTest1 {
-    private final By WEB_ARTICLE_LIST = By.xpath(".//a[@class='top2012-title']");
-    private final By MOB_ARTICLE_LIST = By.xpath(".//a[@class='md-scrollpos']");
+    private final By WEB_ARTICLES_LIST = By.xpath(".//a[@class='top2012-title']");
+    private final By MOB_ARTICLES_LIST = By.xpath(".//a[@class='md-scrollpos']");
 
     @Test
-    public void delfiFiveTitleTest() {
+    public void delfiFiveTitlesTest() {
         System.setProperty("webdriver.chrome.driver", "c:/chromedriver.exe");
         WebDriver browser = new ChromeDriver();
         browser.manage().window().maximize();
         browser.get("http://rus.delfi.lv/");
 
-        List<WebElement> webArticleList = browser.findElements(WEB_ARTICLE_LIST);
-        List<String> webArticleText = new ArrayList<String>();
+        List<WebElement> webArticlesList = browser.findElements(WEB_ARTICLES_LIST);
+        List<String> webArticlesText = new ArrayList<String>();
 
         int i;
         for (i = 0; i < 5; i++) {
-            webArticleText.add(webArticleList.get(i).getText());
+            webArticlesText.add(webArticlesList.get(i).getText());
         }
 
         browser.get("http://m.rus.delfi.lv/");
 
-        List<WebElement> mobArticleList = browser.findElements(MOB_ARTICLE_LIST);
-        List<String> mobArticleText = new ArrayList<String>();
+        List<WebElement> mobArticlesList = browser.findElements(MOB_ARTICLES_LIST);
+        List<String> mobArticlesText = new ArrayList<String>();
         for (i = 0; i < 5; i++) {
-            mobArticleText.add(mobArticleList.get(i).getText());
+            mobArticlesText.add(mobArticlesList.get(i).getText());
         }
 
-        Assertions.assertArrayEquals(new List[]{webArticleText}, new List[]{mobArticleText}, "articles not equals");
+        Assertions.assertArrayEquals(new List[]{webArticlesText}, new List[]{mobArticlesText}, "articles not equals");
         browser.close();
     }
 }
