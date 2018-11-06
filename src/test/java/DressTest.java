5413ee8ca48302537a82def2d86440be18427663
Wiland <jurijs.uljanovs@gmail.com>
2018-11-06 12:56:49 +0200

    initial commit 23

--- src/test/java/DressTest.java
+++ src/test/java/DressTest.java
@@ -12,33 +12,46 @@ public class DressTest {
     private final By WONEN_FILTER_MENU = By.xpath(".//ul[@id='ul_layered_id_attribute_group_3']");
 
     @Test
-    public void orangeDressTest(){
+    public void orangeDressTest() {
         System.setProperty("webdriver.chrome.driver", "c:/chromedriver.exe");
         WebDriver browser = new ChromeDriver();
         browser.manage().window().maximize();
         browser.get("http://automationpractice.com/index.php");
 
         List<WebElement> mainMenuList = browser.findElements(MAIN_MENU);
-boolean qwert = false;
+
+        boolean menuAssert = false;
         for (WebElement element : mainMenuList) {
 //            System.out.println(element.getText());
             if (element.getText().equals("WOMEN")) {
                 element.click();
-                qwert = true;
+                menuAssert = true;
                 break;
             }
         }
-        Assertions
+        Assertions.assertTrue(menuAssert, "Menu Element not found");
 
         List<WebElement> womenMenuList = browser.findElements(WONEN_FILTER_MENU);
+
         for (int i = 0; i < womenMenuList.size(); i++) {
             System.out.println(womenMenuList.get(i).getText());
+        }
+
+        boolean qwert = false;
+        for (WebElement element : mainMenuList) {
+//            System.out.println(element.getText());
+            if (element.getText().contains("Orange")) {
+                element.click();
+                qwert = true;
+                break;
+            }
+            Assertions.assertTrue(qwert, "Menu Element not found");
+
         }
 
 
 //        browser.close();
     }
-
 
 
 }
